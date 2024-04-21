383e57a0d360bab6c9da779664877d6bf3551c82
Deividmattos <deividmattos79@hotmail.com>
2024-04-21 19:56:27 +0100

    Create Js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,27 @@
-$(document).ready(function () {
-  $("#menu").click(function () {
-    $(this).toggleClass("fa-times");
-    $("header").toggleClass("toggle");
+const menuIcon = document.querySelector("#menu-icon");
+const navbar = document.querySelector(".navbar");
+
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelector("header nav a");
+
+window.onscroll = () => {
+  section.forEach((sec) => {
+    let top = window.scrollY;
+    let offset = sec.offset - 150;
+    let height = sec.offsetHeight;
+
+    if (top >= offset && top < offset + height) {
+      navLinks.forEach((links) => {
+        links.classList.remove("active");
+        document
+          .querySelector("header nav a[href*=" + id + "]")
+          .classList.add("active");
+      });
+    }
   });
-  $(window).on("scroll load", function () {
-    $("#menu").removeClass("fa-times");
-    $("header").removeClass("toggle");
-  });
-});
+};
+
+menuIcon.onclick = () => {
+  menuIcon.classList.toggle("bx-x");
+  navbar.classList.toggle("active");
+};
