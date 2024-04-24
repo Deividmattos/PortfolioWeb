4e635de9fac5e1b7ad24d136ede2026ce39f944d
Deividmattos <deividmattos79@hotmail.com>
2024-04-24 20:49:32 +0100

    Menu hamburgue

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+const menuList = document.querySelector("#menuList");
+menuList.style.maxHeight = "0px";
+
+function toggleMenu() {
+  if (menuList.style.maxHeight == "0px") {
+    menuList.style.maxHeight = "300px";
+  } else {
+    menuList.style.maxHeight = "0px";
+  }
+}
